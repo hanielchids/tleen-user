0a616ed202ee5e2ab96c706f181ce876ff2b8723
Haniel Chidavose <Haniel.Chidavose@sovtech.com>
2022-12-21 19:31:37 +0200

    feat(ios): ios configuration

--- TleenClient/ios/TleenClient/AppDelegate.mm
+++ TleenClient/ios/TleenClient/AppDelegate.mm
@@ -4,6 +4,8 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <GoogleMaps/GoogleMaps.h>
+
 #import <React/RCTAppSetupUtils.h>
 
 #if RCT_NEW_ARCH_ENABLED
@@ -31,6 +33,8 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyAz48wFGKcLYPQIhaLTX_Vh8FVSzUUBYHE"];
+
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
