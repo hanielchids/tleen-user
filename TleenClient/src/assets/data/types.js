262952e3369beca02e3f3d70a732ca1e3f273cef
Haniel Chidavose <Haniel.Chidavose@sovtech.com>
2022-12-21 18:45:25 +0200

    feat(assets): added assets

---
+++ TleenClient/src/assets/data/types.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 0,
+    type: 'TLEEN',
+    price: 220,
+    duration: 45,
+  },
+  {
+    id: 1,
+    type: 'TLEEN XTRA',
+    price: 270,
+    duration: 30,
+  },
+  {
+    id: 2,
+    type: 'TLEEN GOLD',
+    price: 360,
+    duration: 27,
+  },
+];
