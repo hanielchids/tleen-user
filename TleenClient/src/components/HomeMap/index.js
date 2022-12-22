b3d65893590030f2def2f8013e8996206af02ccf
Haniel Chidavose <Haniel.Chidavose@sovtech.com>
2022-12-22 10:07:34 +0200

    feat(map): map component added

--- TleenClient/src/components/HomeMap/index.js
+++ TleenClient/src/components/HomeMap/index.js
@@ -1,17 +1,18 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const HomeMap = () => {
   return (
-    <View
-      style={{
-        height: 300,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'lightblue',
-      }}>
-      <Text>Map</Text>
-    </View>
+    <MapView
+      provider={PROVIDER_GOOGLE}
+      style={{height: '100%', width: '100%'}}
+      region={{
+        latitude: -26.107567, // change it to origin.location.lat
+        longitude: 28.056702, // change it to origin.location.lng
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+      }}></MapView>
   );
 };
 
