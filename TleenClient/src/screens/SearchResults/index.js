10de8326fd1abf0403b4d396b4cb5157210840f3
Haniel Chidavose <Haniel.Chidavose@sovtech.com>
2022-12-22 10:08:14 +0200

    feat(screen): updated map screen with ride options

--- TleenClient/src/screens/SearchResults/index.js
+++ TleenClient/src/screens/SearchResults/index.js
@@ -5,9 +5,14 @@ import RideOptions from '../../components/RideOptions';
 
 const SearchResults = () => {
   return (
-    <View>
-      <HomeMap />
-      <RideOptions />
+    <View style={{display: 'flex'}}>
+      <View style={{height: '50%'}}>
+        <HomeMap />
+      </View>
+
+      <View style={{height: '50%'}}>
+        <RideOptions />
+      </View>
     </View>
   );
 };
