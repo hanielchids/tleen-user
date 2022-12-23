8b234d42817e71f4cdb6f93dea51cfa1443424af
Haniel Chidavose <Haniel.Chidavose@sovtech.com>
2022-12-23 18:55:31 +0200

    fix(home): updated homescreen

--- TleenClient/src/components/HomeSearch/index.js
+++ TleenClient/src/components/HomeSearch/index.js
@@ -1,22 +1,29 @@
-import {View, Text} from 'react-native';
+import {View, Text, Pressable} from 'react-native';
 import React from 'react';
 import styles from './styles';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Entypo from 'react-native-vector-icons/Entypo';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {useNavigation} from '@react-navigation/native';
 
 const HomeSearch = () => {
+  const navigation = useNavigation();
+
+  const goToSearch = () => {
+    navigation.navigate('DestinationSearch');
+  };
+
   return (
     <View>
       {/* Input Box */}
-      <View style={styles.inputBox}>
+      <Pressable onPress={goToSearch} style={styles.inputBox}>
         <Text style={styles.inputText}>Where to?</Text>
         <View style={styles.timeContainer}>
           <AntDesign name="clockcircle" color="#535353" size={16} />
           <Text>Now</Text>
           <MaterialIcons name="keyboard-arrow-down" size={16} />
         </View>
-      </View>
+      </Pressable>
 
       {/* Previous destination */}
 
