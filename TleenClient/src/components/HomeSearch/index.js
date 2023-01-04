67e8bfdf8baa3e6f6f874d09795a64219fd7c42e
Haniel Chidavose <Haniel.Chidavose@sovtech.com>
2023-01-04 23:51:25 +0200

    feat(screens): added client info to homescreen

--- TleenClient/src/components/HomeSearch/index.js
+++ TleenClient/src/components/HomeSearch/index.js
@@ -1,20 +1,40 @@
 import {View, Text, Pressable} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './styles';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Entypo from 'react-native-vector-icons/Entypo';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {useNavigation} from '@react-navigation/native';
+import {Auth} from 'aws-amplify';
 
 const HomeSearch = () => {
   const navigation = useNavigation();
+  const [username, setUsername] = useState('');
 
   const goToSearch = () => {
     navigation.navigate('DestinationSearch');
   };
 
+  useEffect(() => {
+    const getUserInfo = async () => {
+      try {
+        const user = await Auth.currentSession();
+        setUsername(user.accessToken.payload.username);
+
+        console.log('user stuff is: ', user.accessToken.payload.username);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getUserInfo();
+  }, []);
+
   return (
     <View>
+      <Text style={{textAlign: 'center', padding: 5, fontSize: 22}}>
+        Hi, {username}
+      </Text>
       {/* Input Box */}
       <Pressable onPress={goToSearch} style={styles.inputBox}>
         <Text style={styles.inputText}>Where to?</Text>
@@ -24,18 +44,14 @@ const HomeSearch = () => {
           <MaterialIcons name="keyboard-arrow-down" size={16} />
         </View>
       </Pressable>
-
       {/* Previous destination */}
-
       <View style={styles.row}>
         <View style={styles.iconContainer}>
           <AntDesign name="clockcircle" color="#fff" size={16} />
         </View>
         <Text style={styles.destinationText}>3 Rivonia Road</Text>
       </View>
-
       {/* Home destination */}
-
       <View style={styles.row}>
         <View style={[styles.iconContainer, {backgroundColor: '#2FA94E'}]}>
           <Entypo name="home" color="#fff" size={16} />
