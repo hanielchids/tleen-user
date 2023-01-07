cea941ea268554f398f997773ad01b7ea70a3c3b
Haniel Chidavose <Haniel.Chidavose@sovtech.com>
2023-01-07 11:46:16 +0200

    feat(app): updated driver app config

--- TleenDriver/App.js
+++ TleenDriver/App.js
@@ -6,13 +6,21 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import type {Node} from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 
 // import Geolocation from '@react-native-community/geolocation';
 import {enableLatestRenderer} from 'react-native-maps';
+
+import {Amplify, Auth, API, graphqlOperation} from 'aws-amplify';
+import awsExports from './src/aws-exports';
+import {withAuthenticator} from '@aws-amplify/ui-react-native';
+import {getCarId} from './src/graphql/queries';
+import {createCar} from './src/graphql/mutations';
+
+Amplify.configure(awsExports);
 
 // Geolocation require statement for backwards compatibility
 // navigator.geolocation = require('@react-native-community/geolocation');
@@ -56,6 +64,39 @@ const App: () => Node = () => {
   //   }
   // }, []);
 
+  useEffect(() => {
+    const updateUserCar = async () => {
+      // Get authenticated user
+
+      const authenticatedUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+      if (!authenticatedUser) {
+        return;
+      }
+
+      // check if the user already has a car
+      const carData = await API.graphql(
+        graphqlOperation(getCarId, {id: authenticatedUser.attributes.sub}),
+      );
+
+      if (!!carData.data.getCar) {
+        console.log('user already has a car assigned');
+        return;
+      }
+
+      // If they don't create a new car for the user
+      const newCar = {
+        id: authenticatedUser.attributes.sub,
+        type: 'TLEEN',
+        userId: authenticatedUser.attributes.sub,
+      };
+      await API.graphql(graphqlOperation(createCar, {input: newCar}));
+    };
+
+    updateUserCar();
+  }, []);
+
   return (
     <SafeAreaView>
       <StatusBar barStyle="dark-content" />
@@ -64,4 +105,4 @@ const App: () => Node = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
